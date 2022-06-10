0df6140cf616b3827f6ceece3a68b8e30a35cdf3
Alif <mrizki.alif@ptap.co.id>
2022-06-10 10:51:14 +0700

    update fungsi promiseOutput

--- promise.js
+++ promise.js
@@ -1,7 +1,22 @@
 const { promiseTheaterIXX, promiseTheaterVGC } = require("./external.js");
 
 // TODO: Buat fungsi promiseOutput sesuai ketentuan readme
-const promiseOutput = null;
+const promiseOutput = async (emosi) => {
+	try {
+		const objects = [
+			...(await promiseTheaterIXX()),
+			...(await promiseTheaterVGC()),
+		];
+		const filteredObject = objects.filter((object) => {
+			if (object.hasil === emosi) {
+				return object;
+			}
+		});
+		return filteredObject.length;
+	} catch (error) {
+		return error;
+	}
+};
 
 module.exports = {
   promiseOutput,
